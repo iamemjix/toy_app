49ef26d97b583d984346c3f6e1e05c9d1fb49e62
iamemjix <gracemary.verzon@gmail.com>
2023-03-16 23:20:53 +0800

    user sign in/up and logout working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    before_action :set_current_user
+
+    def set_current_user
+        if session[:user_id]
+            #finding user by column (find_by)
+            Current.user = User.find_by(id: session[:user_id])
+        end   
+    end
 end
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,4 @@
 class HomeController < ApplicationController
-    def index
-        if session[:user_id]
-            #finding user by column (find_by)
-            @user = User.find_by(id: session[:user_id])
-        end    
+    def index 
     end
 end
