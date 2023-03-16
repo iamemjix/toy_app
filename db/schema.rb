16f484ec0cefc0396389216655bfa22cdb573dee
iamemjix <gracemary.verzon@gmail.com>
2023-03-17 02:20:39 +0800

    added toy db model

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_16_103840) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_180852) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "toys", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.string "name"
+    t.string "category"
+    t.text "description"
+    t.string "img_id"
+    t.string "img"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_toys_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -21,4 +33,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_103840) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "toys", "users"
 end
