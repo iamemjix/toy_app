94791eb30060b8778ffbe42ad2530a1faaf6f452
iamemjix <gracemary.verzon@gmail.com>
2023-03-16 19:28:35 +0800

    working sign up

---
+++ app/controllers/registration_controller.rb
@@ -0,0 +1,28 @@
+class RegistrationController < ApplicationController
+    def new_user
+        @user = User.new
+    end
+
+    #getting user's info in parameters then inserting it to the model
+    def create
+        respond_to do |format|
+            @user = User.new(user_params)
+    
+            if @user.save
+                #This is for cookies
+                session[:user_id] = @user.id
+                #Redirection once account created.
+                format.html { redirect_to root_path, notice: "Account created successfully!"}
+            else 
+                format.html { render "registration/new_user", status: :unprocessable_entity }
+            end
+        end
+    end
+
+    private
+
+    #raise an error if the user is not found. only permitting user to get ff info.
+    def user_params
+        params.require(:user).permit(:email, :password, :password_confirmation) 
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,9 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+
+  root "home#index"
+  get "sign_up", to: "registration#new_user"
+  post "sign_up", to: "registration#create"
+
 end
