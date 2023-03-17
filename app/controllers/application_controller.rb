57ff4cd9ade6f7ae8e520bcd3bf7f4a745bb1361
iamemjix <gracemary.verzon@gmail.com>
2023-03-17 22:21:02 +0800

    working create toy +cloudinary

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,15 @@ class ApplicationController < ActionController::Base
             Current.user = User.find_by(id: session[:user_id])
         end   
     end
+
+    def req_user_signed_in
+        if Current.user.nil?
+            redirect_to sign_in_path, notice: "User must be logged in."
+        end
+    end
+
+    def req_cloudinary
+        require './config'
+        require 'json'
+    end
 end
--- app/controllers/toy_controller.rb
+++ app/controllers/toy_controller.rb
@@ -1,14 +1,24 @@
 class ToyController < ApplicationController
+    before_action :req_user_signed_in
+    before_action :req_cloudinary
+
     def new
         @toy = Toy.new
     end
 
     def create
+        #checking if toy image is present in params
+        if params[:toy][:img].present?
+            upload_response = Cloudinary::Uploader.upload(params[:toy][:img])
+            params[:toy][:img] = upload_response["secure_url"]
+            params[:toy][:img_id] = upload_response["public_id"]
+        end   
+
         respond_to do |format|
             @toy = Toy.new(toy_params)
 
             if @toy.save
-                format.html { redirect_to toy_url(@toy), notice:"Toy created successfully." }
+                format.html { redirect_to root_path, notice:"Toy created successfully." }
             else
                 format.html { render "toy/new", status: :unprocessable_entity }
             end
@@ -29,6 +39,6 @@ class ToyController < ApplicationController
 
     private
     def toy_params
-        params.require(:toy).permit(:name, :category, :img, :description)
+        params.require(:toy).permit(:name, :category, :img, :img_id, :description, :user_id) #lagay img id tyaka user id
     end
 end
