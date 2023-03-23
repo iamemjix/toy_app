6014fd0efcde99a6c3a3b6a31c09402ba3b81c5a
iamemjix <gracemary.verzon@gmail.com>
2023-03-24 00:51:00 +0800

    working edit

--- app/controllers/toy_controller.rb
+++ app/controllers/toy_controller.rb
@@ -4,6 +4,7 @@ class ToyController < ApplicationController
 
     def new
         @toy = Toy.new
+        @image = Image.new
     end
 
     def edit
@@ -41,8 +42,6 @@ class ToyController < ApplicationController
     end
 
     def create
-        @toy = Current.user.toys.new(toy_params.except(:image))
-
         #checking if toy image is present in params
         if params[:toy].has_key?(:image) && params[:toy][:image][:img].empty? == false
             image_params = []
@@ -53,33 +52,52 @@ class ToyController < ApplicationController
                 image_hash = {img: upload_response["secure_url"], img_id: upload_response["public_id"]}
                 image_params.push(image_hash)
             end
-
-            image_params.each do |img|
-                @image = @toy.images.new(img)
-                @image.save
-            end
         end
 
         respond_to do |format|
-            if @toy.save
-                format.html { redirect_to root_path, notice: "Toy created successfully." }
+            @toy = Current.user.toys.new(toy_params.except(:image))
+
+            if @toy.save && image_params.present?
+                image_params.each do |img|
+                    @image = @toy.images.new(img)
+                    @image.save
+                end
+
+                format.html { redirect_to toy_list_path, notice: "Toy created successfully." }
             else
-                format.html { redirect_to create_toy_path, status: :unprocessable_entity, notice: "Error occured. Please retry."}
+                format.html { redirect_to create_toy_path, status: :unprocessable_entity, notice: "Error occured. Please retry." }
             end
         end
     end
 
     def update
         @toy = Toy.find_by(id: params[:id])
-        if params[:toy][:img].present?
-            Cloudinary::Uploader.destroy(@toy.img_id)
-            upload_response = Cloudinary::Uploader.upload(params[:toy][:img])
-            params[:toy][:img] = upload_response["secure_url"]
-            params[:toy][:img_id] = upload_response["public_id"]
-        end 
+        @image = Image.where(toy_id: @toy.id)
+
+        image_params = []
+
+        if params[:toy][:image][:img].present?
+            @image.each do |imageDestroy|
+                Cloudinary::Uploader.destroy(imageDestroy.img_id)
+            end
+            #deleting record
+            Image.where(toy_id: @toy.id).destroy_all
+
+            params[:toy][:image][:img].each do |image|
+                upload_response = Cloudinary::Uploader.upload(image)
+
+                image_hash = {img: upload_response["secure_url"], img_id: upload_response["public_id"]}
+                image_params.push(image_hash)
+            end
+        end
 
         respond_to do |format|
-            if @toy.update(toy_params)
+            if @toy.update(toy_params) && image_params.present?
+                image_params.each do |img|
+                    @image = @toy.images.new(img)
+                    @image.save
+                end
+
                 format.html { redirect_to toy_list_path, notice:"Toy updated successfully." }
             else
                 format.html { render "toy/edit", status: :unprocessable_entity }
--- app/models/toy.rb
+++ app/models/toy.rb
@@ -2,8 +2,8 @@ class Toy < ApplicationRecord
   belongs_to :user
   has_many :images
 
-  validates :name, presence: :true
-  validates :category, presence: :true
-  validates :description, presence: :true
+  validates :name, presence: {message: "Name is empty."}
+  validates :category, presence: {message: "Category is empty."}
+  validates :description, presence: {message: "Description is empty."}
 end
 
